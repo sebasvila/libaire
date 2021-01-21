6753c90bde723fd4ea4ea6df6f531e11ae31736a
sebasvila <svilamarta@gmail.com>
2021-01-21 18:21:35 +0100

    Reorganize to better suit RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -1,6 +1,5 @@
 import subprocess, os
 
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -8,9 +7,8 @@ import subprocess, os
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 
-# -- Check if RDT build
+# -- Check if this is an RDT build
 read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
-
 
 
 # -- Path setup --------------------------------------------------------------
@@ -27,8 +25,8 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 # -- Project information -----------------------------------------------------
 
 project = 'libaire'
-copyright = '2020, Sebas Vila and others'
-author = 'Sebas Vila and others'
+copyright = '2020, Sebas Vila, Pau Lafoz, Paco del Aguila and others'
+author = 'Sebas Vila, Pau Lafoz, Paco del Aguila and others'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
@@ -59,18 +57,21 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+if read_the_docs_build:
+    html_theme = "sphinx_rtd_theme",
+    extensions.append(html.theme)
+else:
+    html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-
-
 # enforce master_doc to cope with old versions
 master_doc = 'index'
 
+# config breathe plugin 
 breathe_projects = { "libaire": "../doxyxml/" }
 breathe_default_project = "libaire"
 breathe_domain_by_extension = {
@@ -78,14 +79,33 @@ breathe_domain_by_extension = {
         "c" : "c",
         }
 
-
-
-html_theme = "classic"
-
-
-
+# specific for RDT builds
 if read_the_docs_build:
+
+    # execute doxygen on RDT server
     subprocess.call('cd ..; doxygen doxygen.conf', shell=True)
-    # Deactivated using rtd plantuml version as it looks quite old.
+
+    # define plantuml on RDT server
     plantuml = 'java -Djava.awt.headless=true -jar /usr/share/plantuml/plantuml.jar'
+    # if you need a local plantuml java executable
     #plantuml = 'java -Djava.awt.headless=true -jar {}'.format(local_plantuml_path)
+
+    # config RTD theme
+    html_theme_options = {
+        'analytics_id': 'UA-XXXXXXX-1',  #  Provided by Google in your dashboard
+        'analytics_anonymize_ip': False,
+        'logo_only': False,
+        'display_version': True,
+        'prev_next_buttons_location': 'bottom',
+        'style_external_links': False,
+        'vcs_pageview_mode': '',
+        'style_nav_header_background': 'white',
+        # Toc options
+        'collapse_navigation': True,
+        'sticky_navigation': True,
+        'navigation_depth': 4,
+        'includehidden': True,
+        'titles_only': False
+    }
+
+    
